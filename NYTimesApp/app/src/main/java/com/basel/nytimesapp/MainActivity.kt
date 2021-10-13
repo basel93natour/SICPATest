929bd667ca5a36564e825aa2d65f49639b2f5f66
LAPTOP-8JIV1185\60116 <baselalnatour27@gmail.com>
2021-10-13 12:08:18 +0800

    create the project

---
+++ NYTimesApp/app/src/main/java/com/basel/nytimesapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.basel.nytimesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
